7736a6ae591bf9ee3b54a17b56ffccb9087434bc
Shaun Kerr <shaundavidkerr@gmail.com>
2023-02-04 13:07:30 +1100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -132,6 +132,25 @@ pub use process_local::statik::Key as __StaticProcessLocalInner;
 pub use process_local::ProcessLocal;
 pub use tag::Tag;
 
+// temporary until merged,
+// discussed here: https://github.com/lunatic-solutions/lunatic/pull/160
+pub mod sqlite {
+    #[link(wasm_import_module = "lunatic::sqlite")]
+    extern "C" {
+        pub fn open(path: *const u8, path_len: usize, conn_id: *mut u32) -> u64;
+        pub fn query_prepare(
+            conn_id: u64,
+            query_str: *const u8,
+            query_str_len: u32,
+            len_ptr: *mut u32,
+            resource_id: *mut u32,
+        ) -> ();
+        pub fn query_result_get(resource_id: u64, write_buf: *const u8, write_buf_len: u32) -> ();
+        pub fn drop_query_result(resource_id: u64) -> ();
+        pub fn execute(conn_id: u64, exec_str: *const u8, exec_str_len: u32) -> u32;
+    }
+}
+
 /// Implemented for all resources held by the VM.
 pub trait Resource {
     /// Returns process local resource ID.
